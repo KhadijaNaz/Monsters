f65cc589248c15d7cec7960f107b94e5839317f2
KHADIJA NAZ <67429399+KhadijaNaz@users.noreply.github.com>
2021-08-23 18:01:30 -0700

    monsters ID
    Adding state, map()

--- src/App.js
+++ src/App.js
@@ -7,21 +7,29 @@ class App extends Component{
   constructor(){
     super();
  this.state={ 
-   string: 'Hello I am a Monster'
+  monsters: [
+    {
+      name: 'Frankestein',
+      id: 'arp1'
+    },
+    {
+      name: 'Dracula',
+      id: 'db2a'
+    },
+    {
+      name: 'zoombie',
+      id: 'yuef3'
+    }
+  ]
  };
   }
   render(){
     return(
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        {this.state.string}
-        </p>
-        <button onClick={()=>this.setState({string:'hello Monster'})}>
-          show detail
-          </button>
-      </header>
+           {
+       this.state.monsters.map(monster =>(<h1 key= { monster.id}> {monster.name}</h1>))
+     }
+
     </div>
     )
   }
