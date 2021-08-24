69e566e3940d9d6f775c7c48f9f5fcee83522667
KHADIJA NAZ <67429399+KhadijaNaz@users.noreply.github.com>
2021-08-23 17:50:08 -0700

    button onClick

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React, { component } from 'react';
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-
-class App extends Component {
-    render() {
-        return ( <
-            div className = "App" >
-            <
-            header className = "App-header" >
-            <
-            img src = { logo }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            p >
-            Hello I am a Monster. <
-            /p> <
-            a className = "App-link"
-            href = "https://reactjs.org"
-            target = "_blank"
-            rel = "noopener noreferrer" >
-            Learn React <
-            /a> <
-            /header> <
-            /div>
-
-        );
-    }
-}
-
-
+import { react } from '@babel/types';
+ 
+class App extends Component{
+  constructor(){
+    super();
+ this.state={ 
+   string: 'Hello I am a Monster'
+ };
+  }
+  render(){
+    return(
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        {this.state.string}
+        </p>
+        <button onClick={()=>this.setState({string:'hello Monster'})}>
+          show detail
+          </button>
+      </header>
+    </div>
+    )
+  }
+};
 export default App;
