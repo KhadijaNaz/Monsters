50dddcf5e91db40a35aef7b9c12d4589b32ef2d4
KHADIJA NAZ <67429399+KhadijaNaz@users.noreply.github.com>
2021-09-02 13:41:54 -0700

    updated props children

--- src/App.js
+++ src/App.js
@@ -21,10 +21,8 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <CardList name="Monster1">
-        {this.state.monsters.map(monster =>(<h1 key= { monster.id}> {monster.name}</h1>))}
-        </CardList>
-         
+        <CardList monsters={this.state.monsters}/>
+       
 
     </div>
     
--- src/Component/CardList/CardList.Component.jsx
+++ src/Component/CardList/CardList.Component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './CardList.styles.css';
 
 export const CardList =props => {
- return<div className= 'card-list'>{props.children}</div>;
+ return<div className= 'card-list'>
+     {props.monsters.map(monster =>(<h1 key= { monster.id}> {monster.name}</h1>))}
+  </div>;
  
 };
