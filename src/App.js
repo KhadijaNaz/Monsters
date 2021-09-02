d5681a0f26dadfa09d79e98f6707de73b1bca26a
KHADIJA NAZ <67429399+KhadijaNaz@users.noreply.github.com>
2021-09-02 08:26:17 -0700

    CardList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import React, {Component} from 'react';
+import { CardList } from './Component/CardList/CardList.Component'
 import './App.css';
 import { react } from '@babel/types';
  
@@ -20,9 +21,8 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-           {
-       this.state.monsters.map(monster =>(<h1 key= { monster.id}> {monster.name}</h1>))
-     }
+        <CardList name="Monster"/>
+           {this.state.monsters.map(monster =>(<h1 key= { monster.id}> {monster.name}</h1>))}
 
     </div>
     )
